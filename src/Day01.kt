b849aa81dab7da7ca45de360e7c0fbdc51e7b43e
Lukmanul Hakim <luckmanul@yahoo.com>
2024-12-02 17:49:45 +0000

    Update Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,75 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val d = Day1();
+    val input = d.readInput()
+
+    d.part1(input)
+    d.part2(input)
+}
+
+class Day1 {
+    fun part1(input: Day1Input) {
+        val leftList = input.sortedLeft
+        val rightList = input.sortedRight
+
+        var total = 0
+        for (i in leftList.indices) {
+            val left = leftList[i]
+            val right = rightList[i]
+            val distance = kotlin.math.abs(left - right)
+            total += distance
+        }
+        println("total = $total")
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: Day1Input) {
+        val leftList = input.left
+        val rightList = input.right
+        val similarityMap = mutableMapOf<Int, Int>()
+
+        for (i in leftList.indices) {
+            val right = rightList[i]
+            similarityMap[right] = similarityMap.getOrDefault(right, 0) + 1
+        }
+        var total = 0
+        for (i in leftList.indices) {
+            val left = leftList[i]
+            val multiplier = similarityMap.getOrDefault(left, 0)
+            total += left * multiplier
+        }
+        println("total = $total")
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    fun readInput(): Day1Input {
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        val lines = readInput("day01")
+        val result = Day1Input()
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+        for (line in lines) {
+            val arr = line!!.split("   ").toTypedArray()
+            if (arr[0].isNotBlank() && arr[1].isNotBlank()) {
+                result.addLeft(arr[0].toInt())
+                result.addRight(arr[1].toInt())
+            }
+        }
+        return result
+    }
+
+    class Day1Input {
+        val left = mutableListOf<Int>()
+        val right = mutableListOf<Int>()
+
+        fun addLeft(value: Int) {
+            left.add(value)
+        }
+
+        fun addRight(value: Int) {
+            right.add(value)
+        }
+
+        val sortedLeft: List<Int>
+            get() = left.sorted()
+
+        val sortedRight: List<Int>
+            get() = right.sorted()
+    }
 }
